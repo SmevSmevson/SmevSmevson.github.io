c059cd4920523dd66d8e458ae943e132e454af20
SmevSmevson <kevpmac1@hotmail.com>
2019-12-20 02:39:08 +0900

    Animate skill bars

---
+++ triggers.js
@@ -0,0 +1,27 @@
+let last_known_scroll_position = 0;
+let ticking = false;
+let skillSection = document.getElementById('skills');
+let skillList = skillSection.getElementsByTagName('li');
+
+initSkillBars();
+window.addEventListener('scroll', getScrollY, { passive: true });
+
+function initSkillBars() {
+    for(let i = 0; i < skillList.length; i++) {
+        let bar = skillList[i].children[1].children[0];
+        let num = skillList[i].children[1].children[1].innerHTML;
+        bar.style = `left: 0%;`;
+    }
+}
+
+function getScrollY() {
+    last_known_scroll_position = window.scrollY;
+    if(last_known_scroll_position > skillSection.offsetTop){
+        for(let i = 0; i < skillList.length; i++) {
+            let bar = skillList[i].children[1].children[0];
+            let num = skillList[i].children[1].children[1].innerHTML;
+            bar.style = `transition-duration: 2400ms; transition-delay: ${i*3}00ms; left: ${num};`;
+        }
+        window.removeEventListener('scroll', getScrollY, { passive: true });
+    }
+}
